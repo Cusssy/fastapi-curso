3b41f1793103b1020050cfd952913849e8465477
Cusssy <71029516+Cusssy@users.noreply.github.com>
2025-12-04 15:58:36 +0100

    Update client routes to use ClienteItemsResponse for improved item handling in API responses

--- routes/cliente.py
+++ routes/cliente.py
@@ -7,17 +7,17 @@ from typing import Optional, List
 from db.dbmanager import get_db
 
 from models.cliente import Cliente
-from schemas.cliente import ClienteSchema, ClienteResponse
+from schemas.cliente import ClienteSchema, ClienteItemsResponse
 
 from crud.cliente import get_clients_crud, create_client_crud, get_client_crud, delete_client_crud, update_client_crud
 
 router = APIRouter(prefix="/clients", tags=["client"])
 
-@router.get("/", response_model=List[ClienteResponse])
+@router.get("/", response_model=List[ClienteItemsResponse])
 def get_clients(db: Session = Depends(get_db)):
     return get_clients_crud(db)
 
-@router.post("/", response_model=ClienteResponse)
+@router.post("/", response_model=ClienteItemsResponse)
 def create_client(cliente: ClienteSchema ,db: Session = Depends(get_db)):
     return create_client_crud(cliente, db)
 
@@ -29,6 +29,6 @@ def get_client(ClientID: int = Path(..., ge=1, description="id del client"), db:
 def delete_client(ClientID: int, db: Session = Depends(get_db)):
     return delete_client_crud(ClientID, db)
 
-@router.put("/{ClientID}", response_model=ClienteResponse)
+@router.put("/{ClientID}", response_model=ClienteItemsResponse)
 def update_client(ClientID: int = Path(..., ge=1, description="id del client"), db: Session = Depends(get_db), datos: ClienteSchema = Body()):
     return update_client_crud(ClientID, db, datos)
