230cb12726f1bae1b7fe17a1142e1d5ae7dfa247
Cusssy <71029516+Cusssy@users.noreply.github.com>
2025-12-04 16:18:06 +0100

    Refactor client routes: reorganize endpoint definitions for clarity and consistency in API response handling

--- routes/cliente.py
+++ routes/cliente.py
@@ -13,22 +13,27 @@ from crud.cliente import get_clients_crud, create_client_crud, get_client_crud, 
 
 router = APIRouter(prefix="/clients", tags=["client"])
 
+# Buscar todos
 @router.get("/", response_model=List[ClienteItemsResponse])
 def get_clients(db: Session = Depends(get_db)):
     return get_clients_crud(db)
 
-@router.post("/", response_model=ClienteItemsResponse)
-def create_client(cliente: ClienteSchema ,db: Session = Depends(get_db)):
-    return create_client_crud(cliente, db)
-
+# Buscar por ID
 @router.get("/{ClientID}")
 def get_client(ClientID: int = Path(..., ge=1, description="id del client"), db: Session = Depends(get_db)):
     return get_client_crud(ClientID, db)
 
+# Crear nuevo
+@router.post("/", response_model=ClienteItemsResponse)
+def create_client(cliente: ClienteSchema ,db: Session = Depends(get_db)):
+    return create_client_crud(cliente, db)
+
+# Eliminar por ID
 @router.delete("/{ClientID}")
 def delete_client(ClientID: int, db: Session = Depends(get_db)):
     return delete_client_crud(ClientID, db)
 
+# Actualizar por ID
 @router.put("/{ClientID}", response_model=ClienteItemsResponse)
 def update_client(ClientID: int = Path(..., ge=1, description="id del client"), db: Session = Depends(get_db), datos: ClienteSchema = Body()):
     return update_client_crud(ClientID, db, datos)
