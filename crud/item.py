43a48e57b8f049dbe86e568615d9755126f2d268
Cusssy <71029516+Cusssy@users.noreply.github.com>
2025-12-05 17:39:02 +0100

    Refactor item CRUD operations: add joined loading for cliente in item retrieval functions

--- crud/item.py
+++ crud/item.py
@@ -1,12 +1,22 @@
 from fastapi import FastAPI, Depends, HTTPException, Path, Body, APIRouter
 
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from models.item import Item
 
 from schemas.cliente_item import ItemSchema, ItemResponse
 
 def get_items_crud(db: Session):
     return db.query(Item).all()
+
+def get_items_clientes_crud(db: Session):
+    return db.query(Item).options(joinedload(Item.cliente)).all()
+
+def get_item_crud(ItemID: int, db: Session):
+    items = db.query(Item).filter(Item.id == ItemID).first()
+    return items
+
+def get_item_cliente_crud(ItemID: int, db: Session):
+    return db.query(Item).options(joinedload(Item.cliente)).filter(Item.id == ItemID).first()
 
 
 def create_item_crud(item: ItemSchema, db: Session):
@@ -20,10 +30,6 @@ def create_item_crud(item: ItemSchema, db: Session):
     db.commit()
     db.refresh(ItemNuevo)
     return ItemNuevo
-
-def get_item_crud(ItemID: int, db: Session):
-    items = db.query(Item).filter(Item.id == ItemID).first()
-    return items
 
 def delete_item_crud(ItemID: int, db: Session):
     item = db.query(Item).filter(Item.id == ItemID).first()
